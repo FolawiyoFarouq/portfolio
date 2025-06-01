57213c079d5183773eda5be325a727c51db91c45
FolawiyoFarouq <folawiyofarouq@gmail.com>
2025-06-01 15:26:38 +0100

    Added typewriting effect

--- script.js
+++ script.js
@@ -19,3 +19,37 @@ function updateTime() {
 }
 setInterval(updateTime, 1000);
 updateTime();
+
+ const titles = ["Web Developer", "Software Engineer", "Learner"];
+  let index = 0;
+  let charIndex = 0;
+  const typingSpeed = 100;
+  const erasingSpeed = 50;
+  const delayBetween = 1500;
+
+  const typewriterElement = document.getElementById("typewriter");
+
+  function type() {
+    if (charIndex < titles[index].length) {
+      typewriterElement.textContent += titles[index].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingSpeed);
+    } else {
+      setTimeout(erase, delayBetween);
+    }
+  }
+
+  function erase() {
+    if (charIndex > 0) {
+      typewriterElement.textContent = titles[index].substring(0, charIndex - 1);
+      charIndex--;
+      setTimeout(erase, erasingSpeed);
+    } else {
+      index = (index + 1) % titles.length;
+      setTimeout(type, typingSpeed);
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    if (titles.length) setTimeout(type, delayBetween);
+  });
